6749bc6e82e3be9b49f0e80895c141a499fe8d2c
Mariyan Karastoyanov <mariyan.karastoyanov@witte-automotive.bg>
2025-04-23 16:21:54 +0300

    export orders in excel

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import OrderListView
+from .views import OrderListView, UpdateOrderStatusView
 
 urlpatterns = [
     path('', OrderListView.as_view(), name='order_list'),
+    path('update-order-status/', UpdateOrderStatusView.as_view(), name='update_order_status'),
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,5 +1,9 @@
+import json
+
 from django.contrib import messages
-from django.views.generic import ListView
+from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.views.generic import ListView, View
 from .models import Order
 from .forms import OrderFilterForm
 
@@ -33,5 +37,28 @@ class OrderListView(ListView):
         context['submitted'] = 'loading_date' in self.request.GET and 'status' in self.request.GET
         if context['submitted'] and not context['orders']:
             messages.info(self.request, "No orders found for the selected date and status.")
+        context['ordersId'] = list(self.get_queryset().values_list('id', flat=True))
+
         return context
+
+
+
+class UpdateOrderStatusView(View):
+    def post(self, request, *args, **kwargs):
+        try:
+            data = json.loads(request.body)
+            order_ids = data.get('orderIds', [])
+
+            if not order_ids:
+                return JsonResponse({'success': False, 'message': 'No order IDs provided.'})
+            updated_count = Order.objects.filter(id__in=order_ids).update(status=1)
+            messages.success(request, f'{updated_count} orders were successfully updated.')
+
+            return JsonResponse({'success': True, 'updated': updated_count})
+
+        except Exception as e:
+            messages.error(request, 'An error occurred while updating orders.')
+            return JsonResponse({'success': False, 'message': str(e)})
+#
+# from django.shortcuts import redirect
 
